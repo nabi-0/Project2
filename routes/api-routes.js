c614c2cb584063b0b27c5241918cfa10b0ea404e
nabi-0 <62216642+nabi-0@users.noreply.github.com>
2020-07-29 19:04:12 -0500

    Yaaay! Sigh up and Log in working!
    All the changes I made are for the Sign in and Log in to work. you can create a user using an email and it will save the password. Make sure you're going through node "server.js"

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,9 +10,7 @@ module.exports = function (app) {
     res.json(req.user);
   });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks tohow we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,otherwise send back an error
   app.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
@@ -23,6 +21,7 @@ module.exports = function (app) {
       })
       .catch(function (err) {
         res.status(401).json(err);
+        console.log(err);
       });
   });
 
@@ -47,9 +46,9 @@ module.exports = function (app) {
     }
   });
 
-  //post for reservations
-  app.post("/api/reservations", function (req, res) {
-    //create another table for reservations
-    db.Reservation.create();
-  });
+  // //post for reservations
+  // app.post("/api/reservations", function (req, res) {
+  //   //create another table for reservations
+  //   db.Reservation.create();
+  // });
 };
