ce134b9267f88a4503ab40f4cbcf2ce957b9fd92
nabi-0 <62216642+nabi-0@users.noreply.github.com>
2020-07-31 17:30:05 -0500

    new stuff
    aprooooveeee meeeee

--- models/post.js
+++ models/post.js
@@ -4,7 +4,6 @@ module.exports = function (sequelize, DataTypes) {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       validate: {
         isEmail: true,
       },
--- routes/post-routes.js
+++ routes/post-routes.js
@@ -7,23 +7,30 @@ module.exports = function (app) {
     // findAll returns all entries for a table when used with no options
     db.Post.findAll({}).then(function (dbPost) {
       console.log({ dbPost });
-      res.render("post", dbPost);
+      res.render("post", { userpost: dbPost });
     });
   });
 
   app.post("/post", function (req, res) {
     console.log("handling post request for url /post");
     console.log(req.body);
-    console.log("request.body.username=", req.body.username);
-    console.log("request.body.title=", req.body.title);
-    console.log("request.body.post=", req.body.post);
+    console.log("request.body.data.username=", req.body.data.username);
+    console.log("request.body.data.title=", req.body.data.title);
+    console.log("request.body.data.post=", req.body.data.post);
     db.Post.create({
-      username: req.body.username,
-      title: req.body.title,
-      post: req.body.post,
-    }).then(function (dbPost) {
-      res.json(dbPost);
-    });
+      username: req.body.data.username,
+      title: req.body.data.title,
+      post: req.body.data.post,
+    })
+      .then(function () {
+        res.sendStatus(200);
+        res.end();
+      })
+      .catch(function (err) {
+        console.log("trouble saving to post table", err);
+        res.sendStatus(500);
+        res.end;
+      });
   });
 
   app.delete("/post/:id", function (req, res) {
